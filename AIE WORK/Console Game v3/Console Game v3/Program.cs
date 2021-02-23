06b52f0e45f9b22b068feb59d60a534e80392a43
Kari <kikidunn2002@gmail.com>
2021-02-24 10:15:44 +1100

    wednesday start

---
+++ AIE WORK/Console Game v3/Console Game v3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.InteropServices;
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.CursorVisible = false;
+		ConsoleKeyInfo input = new ConsoleKeyInfo();
+		bool quit = false;
+		new Thread(() =>
+		{
+			Thread.CurrentThread.IsBackground = true;
+			//input = Console.ReadKey();
+			while (input.Key != ConsoleKey.Q)
+			{
+				input = Console.ReadKey();
+				//input2 = Console.Read();
+			}
+			quit = true;
+		}).Start();
+		new Thread(() =>
+		{
+			Thread.CurrentThread.IsBackground = true;
+			while (!quit)
+			{
+				switch (input.Key)
+				{
+					case ConsoleKey.W:
+
+						input = new ConsoleKeyInfo();
+						break;
+					case ConsoleKey.A:
+
+						input = new ConsoleKeyInfo();
+						break;
+					case ConsoleKey.S:
+
+						input = new ConsoleKeyInfo();
+						break;
+					case ConsoleKey.D:
+
+						input = new ConsoleKeyInfo();
+						break;
+				}
+			}
+		}).Start();
+		while (!quit)
+		{
+
+		}
+	}
+}
+
